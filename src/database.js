03b96ed9670a9a741e547cd87fe43f3eba01eb76
Ruben Toro <rubensdt01@gmail.com>
2020-04-27 01:56:06 -0500

    Conectado con base de datos en Mlab

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/notes-db-app', {
+mongoose.connect('mongodb+srv://rubens:afgfexon@cluster0-nzfaq.mongodb.net/test?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
